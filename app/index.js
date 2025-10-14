c6ef292b1de951f066c02efe03bc74266aa06411
vinayjain2408 <vinay98292jain@gmail.com>
2025-10-15 02:48:37 +0530

    remove error on public url

--- app/index.js
+++ app/index.js
@@ -21,21 +21,21 @@ const store = configureStore({
 });
 
 sagaMiddleware.run(rootSaga);
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/firebase-messaging-sw.js')
-      .then((registration) => {
-        console.log(
-          'Service Worker registered with scope:',
-          registration.scope,
-        );
-      })
-      .catch((err) => {
-        console.error('Service Worker registration failed:', err);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('/firebase-messaging-sw.js')
+//       .then((registration) => {
+//         console.log(
+//           'Service Worker registered with scope:',
+//           registration.scope,
+//         );
+//       })
+//       .catch((err) => {
+//         console.error('Service Worker registration failed:', err);
+//       });
+//   });
+// }
 
 root.render(
   <Provider store={store}>
